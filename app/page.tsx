002ff7d18fbfd1332cc7a279c5b4fe4839e3d40a
nehru00123@gmail.com <nehru00123@gmail.com>
2026-05-10 04:17:55 +0530

    feat: implement landing page layout with main sections and global components

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <DailyPrograms />
         <LocationCard />
         <GetInvolved />
-        <OtherWebsites />
+        {/* <OtherWebsites /> */}
       </main>
 
       <Footer />
